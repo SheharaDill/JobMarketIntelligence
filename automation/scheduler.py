b39ad224ee848b4c99ae516d199d45321085d2ef
Shehara Dilshika <shehara5600@gmail.com>
2026-06-30 19:17:05 +0530

    Fix scheduler frequency

--- automation/scheduler.py
+++ automation/scheduler.py
@@ -166,7 +166,7 @@ def start_scheduler():
         scheduler.add_job(
             daily_job_collection,
             trigger="interval",
-            minutes=59,
+            minutes=10,
             id="test_scrape"
         )
         print("Scrape job added.")
@@ -178,7 +178,7 @@ def start_scheduler():
         scheduler.add_job(
             daily_email_report,
             trigger="interval",
-            minutes=59,
+            minutes=30,
             id="test_email"
         )
 
